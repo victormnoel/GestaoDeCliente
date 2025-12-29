02376aca1dd39501f754a55398a5af9efbcdc57c
victormnoel <victor-siqueira01@hotmail.com>
2025-12-29 02:12:04 -0300

    [Add] Adiciona a Query e o Handler relacionados a busca pelo cliente atraves do Id.

---
+++ GestaoDeCliente.Application/Queries/Buscar/BuscarClientePorIdQuery.cs
@@ -0,0 +1,15 @@
+﻿using GestaoDeCliente.Application.Modelos.ViewModels;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestaoDeCliente.Application.Queries.Buscar
+{
+    public class BuscarClientePorIdQuery : IRequest<ClienteViewModel?>
+    {
+        public int ClienteId { get; init; }
+    }
+}
---
+++ GestaoDeCliente.Application/Queries/Buscar/BuscarClientePorIdQueryHandler.cs
@@ -0,0 +1,39 @@
+﻿using GestaoDeCliente.Application.Modelos.ViewModels;
+using GestaoDeCliente.Core.Entidades;
+using GestaoDeCliente.Core.Repositorios;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestaoDeCliente.Application.Queries.Buscar
+{
+    public class BuscarClientePorIdQueryHandler : IRequestHandler<BuscarClientePorIdQuery, ClienteViewModel?>
+    {
+        #region Propriedades
+        private readonly IClienteRepositorio _clienteRepositorio;
+        #endregion
+
+        #region Construtor
+
+        public BuscarClientePorIdQueryHandler(IClienteRepositorio clienteRepositorio)
+        {
+            _clienteRepositorio = clienteRepositorio;
+        }
+        #endregion
+
+        #region Servico
+
+        public async Task<ClienteViewModel?> Handle(BuscarClientePorIdQuery request, CancellationToken cancellationToken)
+        {
+            Cliente? clienteEncontrado = await _clienteRepositorio.BuscarClientePorId(request.ClienteId);
+            return clienteEncontrado is null 
+                ? null 
+                : new ClienteViewModel(clienteEncontrado);
+        }
+
+        #endregion
+    }
+}
