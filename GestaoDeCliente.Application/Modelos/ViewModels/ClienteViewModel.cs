0d2429291d5ca07a626fad79ea972d034fa170e5
victormnoel <victor-siqueira01@hotmail.com>
2025-12-29 02:14:11 -0300

    [Add] Adiciona o Dto ClienteViewModel para retorna ao endpoint de busca apenas os dados necessários.

---
+++ GestaoDeCliente.Application/Modelos/ViewModels/ClienteViewModel.cs
@@ -0,0 +1,28 @@
+﻿using GestaoDeCliente.Core.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestaoDeCliente.Application.Modelos.ViewModels
+{
+    public class ClienteViewModel
+    {
+        #region Propriedades
+        public int ClienteId { get; set; }
+        public string NomeFantasia { get; set; } = string.Empty;
+        public string cnpj { get; set; } = string.Empty;
+
+        #endregion
+
+        #region Construtor
+        public ClienteViewModel(Cliente cliente)
+        {
+            ClienteId = cliente.ClienteId;
+            NomeFantasia = cliente.NomeFantasia;
+            cnpj = cliente.Cnpj.NumeroFormatado;
+        }
+        #endregion
+    }
+}
