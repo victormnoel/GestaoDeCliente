375b6f6ba1f89c4eb4dd9e00a8413dd9b042527c
victormnoel <victor-siqueira01@hotmail.com>
2025-12-29 02:11:21 -0300

    [Add] Adiciona o Command e Handler relacionados ao cadastro de cliente.

---
+++ GestaoDeCliente.Application/Commands/Criar/CriarClienteCommand.cs
@@ -0,0 +1,17 @@
+﻿using GestaoDeCliente.Application.Modelos.ViewModels;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestaoDeCliente.Application.Commands.Criar
+{
+    public class CriarClienteCommand : IRequest<ResultadoDaOperacaoViewModel>
+    {
+        public string NomeFantasia { get; set; }
+        public string Cnpj { get; set; }
+
+    }
+}
