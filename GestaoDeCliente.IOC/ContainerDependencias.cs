d2d7b7356150cb010616f916c52a5e2bd2fd36f6
victormnoel <victor-siqueira01@hotmail.com>
2025-12-29 02:24:32 -0300

    [Add] Adiciona a camada extra GestaoDeCliente.IOC, para centralizar a configuração de dependências.

---
+++ GestaoDeCliente.IOC/ContainerDependencias.cs
@@ -0,0 +1,43 @@
+﻿using GestaoDeCliente.Core.Repositorios;
+using GestaoDeCliente.Core.Servicos;
+using GestaoDeCliente.infrastructure.Repositorios;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestaoDeCliente.IOC
+{
+    public static class ContainerDependencias
+    {
+
+        public static IServiceCollection AdicionarServicos(this IServiceCollection servicos)
+        {
+            #region Conexao com banco de dados
+
+            #endregion
+
+            #region Repostorios
+
+            servicos.AddScoped<IClienteRepositorio, ClienteRepositorio>();
+
+            #endregion
+
+            #region Servico de Dominio
+
+            servicos.AddScoped<IClienteServico, ClienteServico>();
+
+            #endregion
+
+            #region Fluent Validation
+
+
+            #endregion
+
+            return servicos;
+        }
+    }
+}
